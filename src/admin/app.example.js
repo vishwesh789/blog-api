ae8cf86c04daa51f8498ce0effc1f8afff0fc7df
forexbuddy <forexbuddy101@gmail.com>
2023-01-29 16:49:09 +0000

    ckeditor added

--- src/admin/app.example.js
+++ src/admin/app.example.js
@@ -1,3 +1,6 @@
+import ckeditor5Dll from "ckeditor5/build/ckeditor5-dll.js";
+import ckeditor5MrkdownDll from "@ckeditor/ckeditor5-markdown-gfm/build/markdown-gfm.js";
+
 const config = {
   locales: [
     // 'ar',
